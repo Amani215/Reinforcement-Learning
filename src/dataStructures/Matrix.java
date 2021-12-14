4d858ccedeff0e59b09ccda9ce3115678bb57b94
Amani215 <amanibrik5@gmail.com>
2021-12-14 15:40:56 +0100

    Refactoring the logic

--- src/dataStructures/Matrix.java
+++ src/dataStructures/Matrix.java
@@ -4,118 +4,159 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Matrix {
-	private int rows, columns;
 	private double[][] data;
+	private int rows,columns;
 	
-	public Matrix(int rows, int columns) throws Exception {
-		if(rows<=0 || columns<=0)
-			throw new Exception("The number of rows and/or columns is invalid.");
+	public Matrix(int rows,int columns) {
+		data = new double[rows][columns];
 		
 		this.rows = rows;
 		this.columns = columns;
-		data = new double[rows][columns];
 		
-		for(int i=0; i<rows; i++) {
-			for(int j=0; j<columns; j++) {
-				data[i][j]= 0 ;
+		for(int i=0; i<rows; i++)
+		{
+			for(int j=0; j<columns; j++)
+			{
+				data[i][j] = 0;
 			}
 		}
 	}
 	
-	public static Matrix fromArray(double[]x) throws Exception
-    {
-        Matrix temp = new Matrix(x.length, 1);
-        
-        for(int i =0; i<x.length; i++)
-            temp.data[i][0]=x[i];
-        
-        return temp;
-        
-    }
-    
-    public List<Double> toArray() {
-        List<Double> temp= new ArrayList<Double>()  ;
-        
-        for(int i=0; i<rows; i++)
-            for(int j=0; j<columns; j++)
-                temp.add(data[i][j]);
-  
-        return temp;
-   }
-    
-	public void add(double x) {
+	public void print()
+	{
+		for(int i=0; i<rows; i++)
+		{
+			for(int j=0; j<columns; j++)
+			{
+				System.out.print(this.data[i][j]+"	");
+			}
+			System.out.println();
+		}
+	}
+	
+	//returns a vertical matrix
+	public static Matrix fromArray(double[] x)
+		{
+			Matrix temp = new Matrix(x.length, 1);
+			
+			for(int i =0; i<x.length; i++)
+				temp.data[i][0] = x[i];
+			
+			return temp;
+			
+		}
+		
+	public List<Double> toList() {
+			List<Double> temp = new ArrayList<Double>()  ;
+			
+			for(int i=0; i<rows; i++)
+			{
+				for(int j=0; j<columns; j++)
+				{
+					temp.add(data[i][j]);
+				}
+			}
+			
+			return temp;
+		}
+		
+	public void add(int a)
+	{
 		for(int i=0; i<rows; i++) {
-			for(int j=0; j<columns; j++) {
-				data[i][j]+= x ;
+			for(int j=0; j<columns; j++){
+				this.data[i][j] += a;
 			}
 		}
 	}
 	
-	public void add(Matrix m) {
-		if(!m.matches(this))
+	public void add(Matrix m)
+	{
+		if(columns!=m.columns || rows!=m.rows) {
+			System.out.println("The number of rows and columns do not match.");
 			return;
+		}
 		
-		for(int i=0; i<rows; i++) {
-			for(int j=0; j<columns; j++) {
-				data[i][j]+= m.data[i][j] ;
+		for(int i=0; i<rows; i++)
+		{
+			for(int j=0; j<columns; j++)
+			{
+				this.data[i][j] += m.data[i][j];
 			}
 		}
 	}
 	
 	public static Matrix subtract(Matrix a, Matrix b) {
-		if(!a.matches(b))
+		Matrix temp=new Matrix(a.rows, a.columns);
+		
+		for(int i=0; i<a.rows; i++)
+		{
+			for(int j=0; j<a.columns; j++)
+			{
+				temp.data[i][j] = a.data[i][j] - b.data[i][j];
+			}
+		}
+		 
+		return temp;
+	}
+
+	public static Matrix multiplyWithDotProduct(Matrix a, Matrix b) {
+		if(a.columns != b.rows) {
+			System.out.print("The number of columns of the firt matrix is not matching the number of rows of the second matrix");
 			return a;
+		}
 		
-		Matrix temp = a;
+		Matrix temp=new Matrix(a.rows, b.columns);
 		
-		for(int i=0; i<a.rows; i++) {
-			for(int j=0; j<a.columns; j++) {
-				temp.data[i][j] = a.data[i][j]-b.data[i][j];
+		for(int i=0; i<temp.rows; i++)
+		{
+			for(int j=0; j<temp.columns; j++)
+			{
+				double sum=0;
+				for(int k=0; k<a.columns; k++)
+				{
+					sum += a.data[i][k] * b.data[k][j];
+				}
+				temp.data[i][j]=sum;
 			}
 		}
 		
 		return temp;
 	}
 	
-	public void multiply(double a) {
-        for(int i=0;i<rows;i++)
-            for(int j=0;j<columns;j++)
-                this.data[i][j]*=a;
-    }
-	
-	public static Matrix multiply(Matrix a, Matrix b) throws Exception {
-		if(a.columns != b.rows) {
-			System.out.print("The number of columns of the firt matrix is not matching the number of rows of the second matrix");
-			return a;
+	public void multiplyElementByElement(Matrix a) {
+		for(int i=0; i<a.rows; i++)
+		{
+			for(int j=0; j<a.columns; j++)
+			{
+				this.data[i][j] *= a.data[i][j];
+			}
 		}
 		
-		Matrix temp = new Matrix(a.rows,b.columns);
-		
-		for(int i=0;i<temp.rows;i++)
-        {
-            for(int j=0;j<temp.columns;j++)
-            {
-                double sum=0;
-                for(int k=0; k<a.columns; k++)
-                    sum+=a.data[i][k]*b.data[k][j];
-                
-                temp.data[i][j]=sum;
-            }
-        }
-        return temp;
 	}
 	
-	public static Matrix transpose(Matrix m) throws Exception {
-		Matrix temp = new Matrix(m.columns,m.rows);
+	public void multiply(double a) {
+		for(int i=0; i<rows; i++)
+		{
+			for(int j=0; j<columns; j++)
+			{
+				this.data[i][j] *= a;
+			}
+		}
 		
-		for(int i=0;i<m.rows;i++)
-        {
-            for(int j=0;j<m.columns;j++)
-            {
-                temp.data[j][i]=m.data[i][j];
-            }
-        }
-        return temp;
+	}
+	
+	public static Matrix transpose(Matrix m) {
+		Matrix temp=new Matrix(m.columns, m.rows);
+		
+		for(int i=0; i<m.rows; i++)
+		{
+			for(int j=0; j<m.columns; j++)
+			{
+				temp.data[j][i] = m.data[i][j];
+			}
+		}
+		
+		return temp;
 	}
 
 	//activation function and its derivative
@@ -129,7 +170,7 @@ public class Matrix {
     	}
     }
     
-    public Matrix dleakyReLU(double alpha) throws Exception {
+    public Matrix dleakyReLU(double alpha){
     	Matrix temp = new Matrix(rows,columns);
     	
     	for(int i=0; i<rows; i++) {
@@ -140,13 +181,4 @@ public class Matrix {
     	
     	return temp;
     }
-    
-	//checks if the matrices have the same sizes
-	private boolean matches(Matrix m) {
-		if(m.rows!=this.rows||m.columns!=this.columns) {
-			System.out.print("The number of rows and columns do not match.");
-			return false;
-		}
-		return true;
-	}
 }
--- src/models/NeuralNetwork.java
+++ src/models/NeuralNetwork.java
@@ -5,70 +5,107 @@ import java.util.List;
 import dataStructures.Matrix;
 
 public class NeuralNetwork {
-	final double ALPHA = 0.1;	//for the leaky ReLU
+	private final double ALPHA = 0.1;	//for the leaky relu
+	private Matrix weights_ih, weights_ho, hiddenBiases, outputBiases;	
+	private double learningRate = 0.01;
 	
-	Matrix inputWeights, hiddenWeights, hiddenBiases, outputBiases;
-	double learningRate = 0.01;
-	
-	public NeuralNetwork(int numOfInputs, int numOfHiddenNodes, int numOfOutputs) throws Exception {
-		inputWeights = new Matrix(numOfInputs, numOfHiddenNodes);
-		hiddenWeights = new Matrix(numOfHiddenNodes, numOfOutputs);
+	public NeuralNetwork(int numOfInputs,int numOfHiddenNodes,int numOfOutputs) {
+		weights_ih = new Matrix(numOfHiddenNodes, numOfInputs); //(2x3)
+		weights_ho = new Matrix(numOfOutputs, numOfHiddenNodes);//(1x2)
 		
-		hiddenBiases = new Matrix(1, numOfHiddenNodes);
-		outputBiases = new Matrix(1, numOfOutputs);
+		hiddenBiases = new Matrix(numOfHiddenNodes, 1);
+		outputBiases = new Matrix(numOfOutputs, 1);
+		
 	}
 	
-	public List<Double> forwardPass(double[] X) throws Exception
-    {
-        Matrix input = Matrix.fromArray(X);						//get the input from the given array
+	public List<Double> forwardPass(double[] input)
+	{
+		Matrix inputMatrix = Matrix.fromArray(input);
+		
+		Matrix hidden = computeHiddenValues(inputMatrix);
+		Matrix output = computeOutputValues(hidden);
+		
+		return output.toList();
+	}
+	
+	public void backPropagate(double [] input, double [] target)
+	{
+		//Feedforward
+		Matrix inputMatrix = Matrix.fromArray(input);
+		
+		Matrix hidden = computeHiddenValues(inputMatrix);
+		Matrix output = computeOutputValues(hidden);
+		
+		Matrix targetMatrix = Matrix.fromArray(target);
+		
+		//error derivative
+		Matrix derror = Matrix.subtract(output, targetMatrix); //-(target-output) (1x1)
+		
+		//output derivative
+		Matrix gradient_o = output.dleakyReLU(ALPHA); //(1x1)
+		
+		//delta
+		Matrix delta_o = derror;
+		delta_o.multiplyElementByElement(gradient_o); //delta = d(error).d(out) (1x1)
+		
+		//change in the weights is delta_o.out_h
+		Matrix hidden_T = Matrix.transpose(hidden); //(1X2)
+		Matrix who_change =  Matrix.multiplyWithDotProduct(delta_o, hidden_T); //(1x1)(1x2) = (1x2)
+		
+		//Calculating error for hidden nodes (taking into account all outputs)
+		Matrix who_T = Matrix.transpose(weights_ho); //(2x1)
+		Matrix hidden_derrors = Matrix.multiplyWithDotProduct(who_T, delta_o); //delta_o instead of d(error)? (2x1)(1x1) = (2x1)
+		
+		//derivative of the hidden nodes
+		Matrix gradient_h = hidden.dleakyReLU(ALPHA);
+		
+		//delta of hidden nodes
+		Matrix delta_h = hidden_derrors;
+		delta_h.multiplyElementByElement(gradient_h);
+		
+		//change in the weights is delta_h.i
+		Matrix i_T = Matrix.transpose(inputMatrix);
+		Matrix wih_change = Matrix.multiplyWithDotProduct(delta_h, i_T);
+		
+		//update the weights (between the hidden layer and the output)
+		who_change.multiply(learningRate);
+		weights_ho.add(who_change);
+		
+		//update the biases of outputs
+		delta_o.multiply(learningRate);
+		outputBiases.add(delta_o);
+		
+		//update the weights (between the input and the hidden layer)
+		wih_change.multiply(learningRate);
+		weights_ih.add(wih_change);
+		
+		//update the biases of the hidden nodes
+		delta_h.multiply(learningRate);
+		hiddenBiases.add(gradient_h);
+		
+	}
+	
+	public void train(double[] input, double[] target)
+	{
+		//do backpropagation 3 times
+		for(int i=0; i<2; i++)
+			this.backPropagate(input, target);
+	}
+	
+	private Matrix computeHiddenValues(Matrix inputMatrix){
+		Matrix hidden = Matrix.multiplyWithDotProduct(weights_ih, inputMatrix);	//(2x3)(3x1)
+        hidden.add(hiddenBiases);									
+        hidden.leakyReLU(ALPHA);									
         
-        Matrix hidden = Matrix.multiply(inputWeights, input);	//multiply inputs by their weights
-        hidden.add(hiddenBiases);								//add the biases
-        hidden.leakyReLU(ALPHA);								//apply the activation function
+        return hidden; //(2x1)
+	}
+	
+	private Matrix computeOutputValues(Matrix hidden){
+		Matrix output = Matrix.multiplyWithDotProduct(weights_ho,hidden);
+        output.add(outputBiases);								
+        output.leakyReLU(ALPHA);								
         
-        Matrix output = Matrix.multiply(hiddenWeights,hidden);	//multiply the result by the hidden weights
-        output.add(outputBiases);								//add the biases
-        output.leakyReLU(ALPHA);								//apply the activation function to get the final result
-        
-        return output.toArray();
-    }
-
-	public void backPropagate(double [] X,double [] Y) throws Exception
-    {
-        Matrix input = Matrix.fromArray(X);
-        Matrix hidden = Matrix.multiply(inputWeights, input);
-        hidden.add(hiddenBiases);
-        hidden.leakyReLU(ALPHA);;
-        
-        Matrix output = Matrix.multiply(hiddenWeights,hidden);
-        output.add(outputBiases);
-        output.leakyReLU(ALPHA);;
-        
-        Matrix target = Matrix.fromArray(Y);
-        
-        Matrix error = Matrix.subtract(target, output);
-        Matrix gradient = output.dleakyReLU(ALPHA);
-        gradient = Matrix.multiply(gradient, error);
-        gradient.multiply(learningRate);
-        
-        Matrix hidden_T = Matrix.transpose(hidden);
-        Matrix who_delta =  Matrix.multiply(gradient, hidden_T);
-        
-        hiddenWeights.add(who_delta);
-        outputBiases.add(gradient);
-        
-        Matrix who_T = Matrix.transpose(hiddenWeights);
-        Matrix hidden_errors = Matrix.multiply(who_T, error);
-        
-        Matrix h_gradient = hidden.dleakyReLU(ALPHA);
-        h_gradient = Matrix.multiply(h_gradient, hidden_errors);
-        h_gradient.multiply(learningRate);
-        
-        Matrix i_T = Matrix.transpose(input);
-        Matrix wih_delta = Matrix.multiply(h_gradient, i_T);
-        
-        inputWeights.add(wih_delta);
-        hiddenBiases.add(h_gradient);
-        
-    }
+        return output;
+	}
+	
 }
